c1ffead36f420a20d7200ace561348428fa51e2b
abhinav3254 <abhinav3254@gmail.com>
2022-12-18 12:13:06 +0530

    first commit

---
+++ app/src/main/java/com/example/doit/FirstActivity.java
@@ -0,0 +1,31 @@
+package com.example.doit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.doit.databinding.ActivityFirstBinding;
+
+public class FirstActivity extends AppCompatActivity {
+    ActivityFirstBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFirstBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        getSupportActionBar().hide();
+
+        binding.startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
